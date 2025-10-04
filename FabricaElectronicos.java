73074ad4ef5a704ab61a3c25ae0a18afc9d7d14a
DavidSVelascoD <david-velasco1@upc.edu.co>
2025-10-04 01:05:47 -0500

    Delete FabricaElectronicos.java

--- FabricaElectronicos.java
+++
@@ -1,8 +0,0 @@
-package JavaBasics.PilotoInventario;
-
-public class FabricaElectronicos implements FabricaAbstracta {
-    @Override
-    public Producto crearProducto() {
-        return new ProductoConcreto("Electrónico", 200, 10);
-    }
-}
