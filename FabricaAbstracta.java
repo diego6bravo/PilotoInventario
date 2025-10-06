a486d18c49ade11aa061b71d39319a32a1d5ee51
diego6bravo <diego.bravo@live.com>
2025-10-05 21:19:13 -0500

    Agregando documentación completa en el Main.java, en singleton, creador & prototipo

--- FabricaAbstracta.java
+++ FabricaAbstracta.java
@@ -3,3 +3,4 @@ package JavaBasics.PilotoInventario;
 public interface FabricaAbstracta {     // Interfaz base para crear nuevos productos.
     Producto crearProducto();       // declaración de Método para crear un producto.
 }
+
